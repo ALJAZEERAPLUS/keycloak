dd94dcaf498cb2390b571ea4898ed0730e5c6d4e
brosaaj <Rosab@aljazeera.net>
2020-10-02 15:56:41 -0300

    :sparkles: Added the SPI info

--- services/src/main/java/org/keycloak/events/admin/AdminEventListenerProviderFactory.java
+++ services/src/main/java/org/keycloak/events/admin/AdminEventListenerProviderFactory.java
@@ -1,4 +1,4 @@
-  
+
 package org.keycloak.events.admin;
 
 import org.keycloak.Config;
@@ -6,9 +6,15 @@ import org.keycloak.events.EventListenerProviderFactory;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ServerInfoAwareProviderFactory;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.jboss.logging.Logger;
 
-public class AdminEventListenerProviderFactory implements EventListenerProviderFactory {
+public class AdminEventListenerProviderFactory implements EventListenerProviderFactory,
+    ServerInfoAwareProviderFactory {
 
     private static final Logger log = Logger.getLogger("org.keycloak.events");
 
@@ -35,5 +41,12 @@ public class AdminEventListenerProviderFactory implements EventListenerProviderF
     @Override
     public String getId() {
         return "admin-listener";
+    }
+
+    @Override
+    public Map<String, String> getOperationalInfo() {
+        Map<String, String> ret = new LinkedHashMap<>();
+        ret.put("Admin Event Listener", "It provides triggers that manage users on external systems. F.x: A users that gets deleted on Keycloak also gets deleted on PagerDuty");
+        return ret;
     }
 }
